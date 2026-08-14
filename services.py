2b0c536bda966b7c6b32b7f3a31af38ca133e5e2
prathimasirigineni-prog <prathima.sirigineni@gmail.com>
2026-08-14 19:51:55 +0530

    Update services.py

--- services.py
+++ services.py
@@ -1,65 +1,9 @@
-from utils import calculate_economy, calculate_run_rate, calculate_strike_rate
-
-
-INNINGS = {
-    1: {
-        "innings_id": 1,
-        "total_runs": 142,
-        "wickets": 4,
-        "legal_balls": 108,
-        "batters": [
-            {
-                "name": "Player A",
-                "runs": 67,
-                "balls": 48,
-                "fours": 7,
-                "sixes": 2
-            },
-            {
-                "name": "Player B",
-                "runs": 39,
-                "balls": 32,
-                "fours": 4,
-                "sixes": 1
-            },
-            {
-                "name": "Player C",
-                "runs": 21,
-                "balls": 18,
-                "fours": 2,
-                "sixes": 0
-            }
-        ],
-        "bowlers": [
-            {
-                "name": "Bowler A",
-                "legal_balls": 24,
-                "runs_conceded": 28,
-                "wickets": 2
-            },
-            {
-                "name": "Bowler B",
-                "legal_balls": 24,
-                "runs_conceded": 31,
-                "wickets": 1
-            },
-            {
-                "name": "Bowler C",
-                "legal_balls": 18,
-                "runs_conceded": 25,
-                "wickets": 1
-            }
-        ],
-        "recent_balls": [
-            "1",
-            "4",
-            "0",
-            "W",
-            "2",
-            "1"
-        ]
-    }
-}
+from utils import (
+    calculate_economy,
+    calculate_run_rate,
+    calculate_strike_rate,
+    get_recent_balls,
+)
 
 
 def get_top_batter(batters):
@@ -82,23 +26,35 @@ def get_top_bowler(bowlers):
     )["name"]
 
 
-def get_innings_summary(innings_id: int):
-    innings = INNINGS.get(innings_id)
+def build_batter_summaries(ball_events):
+    batter_data = {}
 
-    if innings is None:
-        return None
+    for ball in ball_events:
+        striker = ball.striker
 
-    legal_balls = innings["legal_balls"]
-    overs = legal_balls / 6
+        if striker not in batter_data:
+            batter_data[striker] = {
+                "name": striker,
+                "runs": 0,
+                "balls": 0,
+                "fours": 0,
+                "sixes": 0,
+            }
 
-    run_rate = calculate_run_rate(
-        innings["total_runs"],
-        overs
-    )
+        batter_data[striker]["runs"] += ball.runs
+
+        if ball.is_legal:
+            batter_data[striker]["balls"] += 1
+
+        if ball.runs == 4:
+            batter_data[striker]["fours"] += 1
+
+        if ball.runs == 6:
+            batter_data[striker]["sixes"] += 1
 
     batters = []
 
-    for batter in innings["batters"]:
+    for batter in batter_data.values():
         strike_rate = calculate_strike_rate(
             batter["runs"],
             batter["balls"]
@@ -110,31 +66,99 @@ def get_innings_summary(innings_id: int):
             "balls": batter["balls"],
             "fours": batter["fours"],
             "sixes": batter["sixes"],
-            "strike_rate": round(strike_rate, 2)
+            "strike_rate": round(strike_rate, 2),
         })
+
+    return batters
+
+
+def build_bowler_summaries(ball_events):
+    bowler_data = {}
+
+    for ball in ball_events:
+        bowler = ball.bowler
+
+        if bowler not in bowler_data:
+            bowler_data[bowler] = {
+                "name": bowler,
+                "legal_balls": 0,
+                "runs_conceded": 0,
+                "wickets": 0,
+            }
+
+        if ball.is_legal:
+            bowler_data[bowler]["legal_balls"] += 1
+
+        bowler_data[bowler]["runs_conceded"] += ball.runs
+
+        if ball.wicket:
+            bowler_data[bowler]["wickets"] += 1
 
     bowlers = []
 
-    for bowler in innings["bowlers"]:
-        bowler_overs = bowler["legal_balls"] / 6
+    for bowler in bowler_data.values():
+        overs = bowler["legal_balls"] / 6
 
         economy = calculate_economy(
             bowler["runs_conceded"],
-            bowler_overs
+            overs
         )
 
         bowlers.append({
             "name": bowler["name"],
-            "overs": round(bowler_overs, 1),
+            "overs": round(overs, 1),
             "runs_conceded": bowler["runs_conceded"],
             "wickets": bowler["wickets"],
-            "economy": round(economy, 2)
+            "economy": round(economy, 2),
         })
 
+    return bowlers
+
+
+def get_innings_summary(innings):
+    if not innings.ball_events:
+        return None
+
+    total_runs = sum(
+        ball.runs
+        for ball in innings.ball_events
+    )
+
+    wickets = sum(
+        1
+        for ball in innings.ball_events
+        if ball.wicket
+    )
+
+    legal_balls = sum(
+        1
+        for ball in innings.ball_events
+        if ball.is_legal
+    )
+
+    overs = legal_balls / 6
+
+    run_rate = calculate_run_rate(
+        total_runs,
+        overs
+    )
+
+    batters = build_batter_summaries(
+        innings.ball_events
+    )
+
+    bowlers = build_bowler_summaries(
+        innings.ball_events
+    )
+
+    recent_balls = get_recent_balls(
+        innings.ball_events
+    )
+
     return {
-        "innings_id": innings["innings_id"],
-        "total_runs": innings["total_runs"],
-        "wickets": innings["wickets"],
+        "innings_id": innings.innings_id,
+        "total_runs": total_runs,
+        "wickets": wickets,
         "legal_balls": legal_balls,
         "overs": round(overs, 1),
         "run_rate": round(run_rate, 2),
@@ -142,5 +166,5 @@ def get_innings_summary(innings_id: int):
         "bowlers": bowlers,
         "top_batter": get_top_batter(batters),
         "top_bowler": get_top_bowler(bowlers),
-        "recent_balls": innings["recent_balls"]
+        "recent_balls": recent_balls,
     }
